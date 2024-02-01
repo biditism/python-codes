a2e596fcd7972a265e276e5c6e96a8a2a9cef26c
biditism <60643754+biditism@users.noreply.github.com>
2024-02-01 14:44:10 +0100

    January Update
    Baum-Pines fitting works for now. Documentations still left.

--- bidit.py
+++ bidit.py
@@ -14,6 +14,7 @@ import pandas as pd
 import os
 import pickle
 
+
 ###################################################
 # Declaration of functions
 ###################################################
@@ -28,7 +29,7 @@ def heaviside(x, amplitude=1, steepness=1, base=1, center=0):
 def overlap_penalty(diff, amount=0, width=1):
     penalty = 1
     for x in diff:
-        penalty = penalty * heaviside(x, amount, width)
+        penalty = penalty * heaviside(x, amount, 1/width)
     return penalty
 
 #Abragam like function with a single Dres
@@ -53,18 +54,17 @@ def T2_decay(tau,T2,A=1,beta=1):
 def single_Dres_dq(tau,Dres,T2,A,beta):
     return abragam(tau,Dres)*T2_decay(tau,T2,A,beta)
 
-
-
-
 #Normal distribution
 def gaussian(x,mu=0,sigma=1,normalize=True):
-    prob= (sigma*np.sqrt(2*np.pi))**(-1) * np.exp((-1/2)*(((x-mu)/sigma)**2))
+    sigma = 0.1* mu * sigma
+    prob = (sigma*np.sqrt(2*np.pi))**(-1) * np.exp((-1/2)*(((x-mu)/sigma)**2))
     if normalize==True: prob = prob / np.trapz(prob,x)
     return prob
 
 #Lognormal distribution
 def lognormal(x,mu=0,sigma=1,normalize=True):
-    prob= (x*sigma*np.sqrt(2*np.pi))**(-1) * np.exp((-1/2)*(((np.log(x)-mu)/sigma)**2))
+    mu = np.log(mu)
+    prob = (x*sigma*np.sqrt(2*np.pi))**(-1) * np.exp((-1/2)*(((np.log(x)-mu)/sigma)**2))
     if normalize==True: prob = prob / np.trapz(prob,x)
     return prob
 
@@ -73,6 +73,18 @@ def lognormal2(x,mu=1,sigma=1,normalize=True):
     prob = gaussian(np.log(x),np.log(mu),sigma,normalize)
     if normalize==True: prob = prob / np.trapz(prob,x)
     return prob
+
+#Get the list of all folder in a location which has a file with fixed ending
+def file_list(ending,root_folder=None):
+    file=[]
+    if root_folder is None:
+        root_folder=os.getcwd()
+    for root,dirs, files in os.walk(root_folder):
+        for each in files:
+            if each.endswith(ending):
+                file.append(each[:-len(ending)])
+    return file
+
 
 #Get the foldername and the file with Baum-Pines data
 def rawdata(filename="BP_303.txt",sample=None):
@@ -87,7 +99,8 @@ def clean(df,cutoff=None,norm_factor=None):
     df['I_ref'] = df['I_ref'] / norm_factor
     df['I_tot'] = df['I_DQ'] + df['I_ref']
 
-    plotmq(df['Time'],df['I_DQ'],df['I_tot'])
+    if cutoff==None:
+        plotmq(df['Time'],df['I_DQ'],df['I_tot'])
     
     if cutoff==None:
         finish=1
@@ -99,11 +112,12 @@ def clean(df,cutoff=None,norm_factor=None):
 
     df_new = df[df['Time'] <= cutoff].copy()
 
-    plotmq(df_new['Time'],df_new['I_DQ'],df_new['I_tot'])
+    if cutoff==None:
+        plotmq(df_new['Time'],df_new['I_DQ'],df_new['I_tot'])
     return df_new
 
 #Scatter plot of multiple files
-def plotmq(tau,*args,y_axis='linear',**kwargs):
+def plotmq(tau,*args,y_axis='linear',save=None,show=True,**kwargs):
     for I in args:
         plt.scatter(tau,I)
     for key,values in kwargs.items():
@@ -112,7 +126,15 @@ def plotmq(tau,*args,y_axis='linear',**kwargs):
     plt.yscale(y_axis)
     xmin, xmax, ymin, ymax = plt.axis()
     plt.ylim(bottom=0.001,top=min(ymax,1))
-    plt.show()
+    if y_axis=='linear':
+        plt.ylim(bottom=0)
+    if save is not None:
+        plt.savefig(save+".pdf", format="pdf", bbox_inches="tight")
+        plt.savefig(save+".png", format="png", bbox_inches="tight")
+    if show:
+        plt.show()
+    else:
+        plt.close()
 
 
 #Grab only required parameters from the parameter set
@@ -150,16 +172,16 @@ def load_object(filename):
 ###################################################
 
 #Tail sustraction
-def tail(df,tau_start=None,tail_model=None,params=None):
+def tail(df,tau_start=None,tail_model=None,params=None,vary_beta=False):
     #Ready the model   
     if tail_model==None: tail_model=lm.Model(T2_decay)
 
     #Ready the parameters
     if params==None:
         params = lm.Parameters() 
-        params.add('A', value=0.1,min=0.05,max=0.9) #Fraction of tail
-        params.add('T2', value=max(df['Time'])/3,min=50,max=500) #T2 of tail
-        params.add('beta', value=1,vary=False) #Stretching exponent
+        params.add('A', value=0.1,min=0.01,max=0.9) #Fraction of tail
+        params.add('T2', value=max(df['Time'])/3,min=5 ,max=500) #T2 of tail
+        params.add('beta', value=1,vary=vary_beta) #Stretching exponent
 
     a = tau_start
     
@@ -182,14 +204,11 @@ def tail(df,tau_start=None,tail_model=None,params=None):
         df['I_SumMQ'] = df['I_DQ'] + df['I_ref'] - T2_decay(df['Time'],values['A'],values['T2'],values['beta']) 
         df['I_nDQ'] = df['I_DQ'] / df['I_SumMQ']
 
-##        tail_fit.plot()
-##        plt.show()
-        
-        
-        plotmq(df['Time'],df['I_tot'],df['I_DQ'],y_axis='log',line=fitted)
+   
+        #plotmq(df['Time'],df['I_tot'],df['I_DQ'],y_axis='log',line=fitted)
 
         
-        plotmq(df['Time'],df['I_tot'],df['I_DQ'],df['I_nDQ'],line=fitted)
+        plotmq(df['Time'],df['I_tot'],df['I_DQ'],df['I_nDQ'],y_axis='log',line=fitted)
 
     ##Plot the result and graph
 
@@ -233,7 +252,7 @@ def model_dq_no_distribution(connectivity):
     return dq
 
 ##Model building for dq curve with distribution    
-def model_dq_distribution(connectivity,distribution=False):
+def model_dq_distribution(connectivity):
             
     temp = [lm.Model(distribution_Dres_dq,prefix=fraction) for fraction in connectivity]
     for a in temp: 
@@ -262,20 +281,20 @@ def update_params(parameter,value_dict):
     return parameter
 
 ##Update bounds to new parameter values
-def update_bounds(parameter,value_dict,change=["Dres" , "T2", "sigma"],width=0.2):
+def update_bounds(parameter,value_dict,change=None,width=0.2):
+    if change==None:
+        change=["Dres" , "T2", "sigma"]
     for key,values in value_dict.items():
         if any(a == key for a in change) :
-                parameter[key].set(min=values*(1-width),max=values*(1+width))
+            parameter[key].set(min=values*(1-width),max=values*(1+width))
     return parameter
 
 ##Fit DQ curve only
 def fit_DQ_only(df,parameter,model,connectivity,**kwagrs):
-
     dq_fitted=model.fit(df['I_DQ'],parameter,tau=df['Time'],**kwagrs)
     
-    dq_fitted.plot(show_init=True,title='I dq only fit')
+    dq_fitted.plot_fit(show_init=True,title='I dq only fit')
 
-    fit_dq=dq_fitted.eval()
     comp_dq=dq_fitted.eval_components()
     plotmq(df['Time'],**comp_dq)
     return dq_fitted
@@ -285,52 +304,71 @@ def fit_tot_only(df,parameter,model,connectivity,**kwagrs):
 
     tot_fitted=model.fit(df['I_tot'],parameter,tau=df['Time'],**kwagrs)
     
-    tot_fitted.plot(show_init=True,title='I total only fit')
+    tot_fitted.plot_fit(show_init=True,title='I total only fit')
 
-    fit_tot=tot_fitted.eval()
     comp_tot=tot_fitted.eval_components()
-    plotmq(df['Time'],y_axis='log', Total=fit_tot,**comp_tot)
+    plotmq(df['Time'],y_axis='log',**comp_tot)
     return tot_fitted
 
 #Definition of DQ curve fitting model
-def fit_simultaneous(parameter,df,model_dq,model_tot,connectivity,T2_penalty,Dres_penalty):
-
-    residual_dq = (df['I_DQ'] - model_dq.eval(parameter,tau=df['Time'])) / max(df['I_DQ'])
+def fit_simultaneous(parameter,df,model_dq,model_tot,connectivity,T2_penalty,Dres_penalty,tail_cutoff,allow_overlap=False):
+    df_truncated=df[df['Time']<tail_cutoff].copy()
+    residual_dq = (df_truncated['I_DQ'] - model_dq.eval(parameter,tau=df_truncated['Time']))  #*0.5/ max(df_truncated['I_DQ'])
 
     residual_tot = df['I_tot'] - model_tot.eval(parameter,tau=df['Time'])
     
     residual = pd.concat([residual_dq,residual_tot])
-
-    residual = residual * extract_and_penalize(parameter,connectivity,T2_penalty,Dres_penalty)
     
+    if allow_overlap==False:
+        residual = residual * extract_and_penalize(parameter,connectivity,T2_penalty,Dres_penalty)
+
+    err_A = 1-sum(grab(parameter,connectivity,"A","Tail_",False))
+    residual = residual * (1+abs(err_A))**4
     return residual #Minimization parameter
 
 #Extract T2 and Dres difference
 def extract_diff(parameter,connectivity):
     diff_T2 = np.diff(grab(parameter,connectivity,"T2","Tail_",False))
 
-    diff_Dres = np.diff(grab(parameter,connectivity,"Dres"))
-        
+    diff_Dres = np.diff(grab(parameter,connectivity,"Dres"))    
     return diff_T2,diff_Dres
 
 #Extract and penalize T2 and Dres difference
 def extract_and_penalize(parameter,connectivity,T2_penalty,Dres_penalty):
-    diff_T2, diff_Dres = extract_diff(parameter,connectivity)
     
-    return 1 * overlap_penalty(diff_T2,**T2_penalty) * overlap_penalty(diff_Dres,**Dres_penalty)
+    diff_T2, diff_Dres = extract_diff(parameter,connectivity)
+    a = 1 * overlap_penalty(diff_T2,**T2_penalty) * overlap_penalty(diff_Dres,**Dres_penalty)
+    return a
 
 #This section contains non standard codes
 ###################################################
 #DQ model curve = abragam multiplied by T2 decay
 def distribution_Dres_dq(tau,Dres,sigma,T2,A,beta):
-    tau_s = tau/1000
-    T2_s = T2/1000
-    Dres_Hz = np.linspace(1,200,2000)
-    prob = lognormal(Dres_Hz,np.log(Dres*1000),sigma)
+    tau_s = tau/Dres_domain['scaling']
+    T2_s = T2/Dres_domain['scaling']
+    Dres_Hz,prob =Dres_prob_distribution(Dres,sigma,**Dres_domain)
     DQ=abragam_dist(tau_s,Dres_Hz,prob)*T2_decay(tau_s,T2_s,A,beta)
     return DQ
 
+def Dres_prob_distribution(mean,sigma,start, end, division, scaling, dist):
+    mean=mean*scaling
+    if dist in [lognormal,lognormal2]:
+        Dres=np.logspace(np.log10(start), np.log10(end),num=division)
+    elif dist in [gaussian]:
+        Dres=np.linspace(start, end,division)
+    prob = dist(Dres,mean,sigma)
+    return Dres,prob
 
+###################################################
+# Declaration of global variables
+###################################################
+
+Dres_domain={'start': 1,
+                'end': 200,
+                'division': 2000,
+                'scaling': 1000,
+                'dist':lognormal2
+        }
 #This section is for testing purpose only
 ###################################################
 
